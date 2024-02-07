b31da7236d7f777547aaa1b199b2629bd8891bb9
Xiao yi Hu <xy1998@live.nl>
2024-02-08 00:21:41 +0100

    changed window sizes for SettingsForm and AboutForm, edited text on about form

--- ScreenDimmer/AboutForm.cs
+++ ScreenDimmer/AboutForm.cs
@@ -37,7 +37,7 @@ namespace ScreenDimmer
             // 
             this.TitleLabel.AutoSize = true;
             this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.TitleLabel.Location = new System.Drawing.Point(7, 6);
             this.TitleLabel.Name = "TitleLabel";
             this.TitleLabel.Size = new System.Drawing.Size(229, 37);
             this.TitleLabel.TabIndex = 0;
@@ -54,7 +54,7 @@ namespace ScreenDimmer
             // VersionLabel
             // 
             this.VersionLabel.AutoSize = true;
-            this.VersionLabel.Location = new System.Drawing.Point(16, 59);
+            this.VersionLabel.Location = new System.Drawing.Point(11, 56);
             this.VersionLabel.Name = "VersionLabel";
             this.VersionLabel.Size = new System.Drawing.Size(42, 13);
             this.VersionLabel.TabIndex = 2;
@@ -63,7 +63,7 @@ namespace ScreenDimmer
             // VersionNumberLabel
             // 
             this.VersionNumberLabel.AutoSize = true;
-            this.VersionNumberLabel.Location = new System.Drawing.Point(55, 59);
+            this.VersionNumberLabel.Location = new System.Drawing.Point(50, 56);
             this.VersionNumberLabel.Name = "VersionNumberLabel";
             this.VersionNumberLabel.Size = new System.Drawing.Size(40, 13);
             this.VersionNumberLabel.TabIndex = 3;
@@ -72,10 +72,10 @@ namespace ScreenDimmer
             // MainLabel
             // 
             this.MainLabel.AutoSize = true;
-            this.MainLabel.Location = new System.Drawing.Point(16, 81);
+            this.MainLabel.Location = new System.Drawing.Point(11, 78);
             this.MainLabel.MaximumSize = new System.Drawing.Size(220, 300);
             this.MainLabel.Name = "MainLabel";
-            this.MainLabel.Size = new System.Drawing.Size(218, 156);
+            this.MainLabel.Size = new System.Drawing.Size(217, 143);
             this.MainLabel.TabIndex = 5;
             this.MainLabel.Text = resources.GetString("MainLabel.Text");
             // 
@@ -83,7 +83,7 @@ namespace ScreenDimmer
             // 
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
-            this.ClientSize = new System.Drawing.Size(255, 261);
+            this.ClientSize = new System.Drawing.Size(240, 234);
             this.Controls.Add(this.MainLabel);
             this.Controls.Add(this.VersionNumberLabel);
             this.Controls.Add(this.VersionLabel);
